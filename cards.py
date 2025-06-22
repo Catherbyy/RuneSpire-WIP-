7a05f5d631b9738e141b1fe510700be77ebe5d8a
Catherby <186341735+Catherbyy@users.noreply.github.com>
2025-06-21 23:07:04 -0700

    Added Dragon Dagger
    Added Dragon Dagger (Line 7):
    
    {"name": "Dragon Dagger", "type": "attack", "cost": 0, "damage": 4}

--- cards.py
+++ cards.py
@@ -4,4 +4,5 @@ cards = [
     {"name": "Wind Strike", "type": "attack", "cost": 1, "damage": 5},
     {"name": "Lobster Heal", "type": "special", "cost": 2, "heal": 8},
     {"name": "Block", "type": "defense", "cost": 1, "block": 5}
+    {"name": "Dragon Dagger", "type": "attack", "cost": 0, "damage": 4}
 ]
